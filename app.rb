46a66ccc5188271e4b7832c05c9e9ea101cc4ad9
Karl Fälth <karl.falth@elev.ga.ntig.se>
2022-02-23 11:34:39 +0100

    user interface and main page work

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ database.results_as_hash = true
 pt = Playtime.new
 # exempel: pt.getdb("cheesemax", "", database)
 get '/' do
-    user_list_with_data = database.execute("select user_name, playtime from users inner join playtime_records on users.user_id = playtime_records.user_id order by playtime_records.id desc")
-    p user_list_with_data
+    @user_list_with_data = database.execute("select user_name, date_time, playtime from users inner join playtime_records on users.user_id = playtime_records.user_id order by playtime_records.user_id asc")
     slim(:index)
+end
+get '/update/:username' do
+    if database.execute("select * from users where user_name=?", params[:username]).first == nil
+        "user isn't registered to the database."
+    else
+        pt.getdb(params[:username], "", database)
+        redirect '/'
+    end
 end
